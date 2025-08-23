3d4d0d10f6e9b7b961e02e824c4247aa67fbed80
gaganjangid73 <gaganjangid02@gmail.com>
2025-08-23 11:52:07 +0530

    common component

---
+++ client/src/components/Comman/checkauth.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useLocation, Navigate } from "react-router-dom";
+
+const CheckAuth = ({ isAuth, user, children }) => {
+  const location = useLocation();
+
+  if (
+    !isAuth &&
+    !(
+      location.pathname.includes("/login") ||
+      location.pathname.includes("/signup")
+    )
+  ) {
+    return <Navigate to="auth/login" />;
+  }
+
+  if (
+    isAuth &&
+    (location.pathname.includes("/login") ||
+      location.pathname.includes("/signup"))
+  ) {
+    if (user?.role === "admin") {
+      return <Navigate to="auth/dashboard" />;
+    } else {
+      return <Navigate to="shop/home" />;
+    }
+  }
+
+  if(isAuth && user?.role !== "admin" && location.pathname.includes("admin")){
+    return <Navigate to= "/unauth-page" />
+  }
+};
+
+export default CheckAuth;
