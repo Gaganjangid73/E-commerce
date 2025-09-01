1c1963890e65277733469e7b9488ecc552d09e86
gaganjangid73 <gaganjangid02@gmail.com>
2025-09-01 18:07:37 +0530

    ..

--- Server/server.js
+++ Server/server.js
@@ -5,11 +5,12 @@ const cors = require('cors');
 require('dotenv').config();
 const authrouter = require('./routes/auth/auth_routes');
 
-// create a database
+// if mongoDB url not found then throw the warning 
 if (!process.env.MONGODB_URI) {
   console.warn('MONGODB_URI is not set. Please configure it in your environment.');
 }
 
+// connect to the mongoDB 
 mongoose
   .connect(process.env.MONGODB_URI || '')
   .then(() => console.log("MongoDB Connected"))
@@ -39,3 +40,7 @@ app.use(
  
 
     app.listen(PORT, ()=> console.log(`Server is running on http://localhost:${PORT}`));
+
+
+    // now i want to work  on many  parts and feature of the this website  
+    // 
