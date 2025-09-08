2ab35b7be7b6f4a728a7facc939d8ad64fdaad9b
GaganJangid <121936713+Gaganjangid73@users.noreply.github.com>
2025-09-08 08:29:39 +0530

    Update store.js

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import Authreducer from "./authSlice";
 
 const store = configureStore({
+ // here we are configure or create your store a and in this we add your  reducer 
   reducer: {
     auth: Authreducer,
   },
